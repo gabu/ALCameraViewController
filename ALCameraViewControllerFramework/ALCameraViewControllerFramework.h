1298fe61b7d015edab03dc0ed77d9d515e9a4456
gabu <tsukada.shouya@gmail.com>
2016-03-30 16:14:37 +0900

    Added a framework target

---
+++ ALCameraViewControllerFramework/ALCameraViewControllerFramework.h
@@ -0,0 +1,19 @@
+//
+//  ALCameraViewControllerFramework.h
+//  ALCameraViewControllerFramework
+//
+//  Created by Shoya Tsukada on 2016/03/30.
+//  Copyright © 2016年 zero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ALCameraViewControllerFramework.
+FOUNDATION_EXPORT double ALCameraViewControllerFrameworkVersionNumber;
+
+//! Project version string for ALCameraViewControllerFramework.
+FOUNDATION_EXPORT const unsigned char ALCameraViewControllerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ALCameraViewControllerFramework/PublicHeader.h>
+
+
